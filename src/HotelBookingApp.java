9a40f90c1b451f55e3a1467ed8993049a407c9b7
akashsingh2127 <akash.vidyagyan11@gmail.com>
2026-03-17 08:59:48 +0530

    Base application setup

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,25 @@
+/**
+ * Book My Stay Application
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * This class represents the entry point of the Hotel Booking System.
+ * It prints the application name and version when the program starts.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class HotelBookingApp {
+
+    /**
+     * Main method – entry point of the application.
+     * JVM starts execution from this method.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+    }
+}
